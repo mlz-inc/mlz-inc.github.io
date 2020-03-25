2b39d3d4539d7871797cdbe93e87975142f38a54
AmDer <dmitriiartemev@yandex.ru>
2020-03-26 02:13:19 +0300

    Some fixes

--- acmp/update.sh
+++ acmp/update.sh
@@ -1,1 +1,4 @@
-./parser_top.py > real_table_source.org 
+#!/usr/bin/env bash
+echo "Parsing table..."
+path=$( git rev-parse --show-toplevel )
+python $path/acmp/parser_top.py > $path/acmp/real_table_source.org 
