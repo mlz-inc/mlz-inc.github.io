78587af3dc40f1fe66632f058236cefe7b3aa9ab
AmDer <dmitriiartemev@yandex.ru>
2020-03-26 17:37:54 +0300

    Auto update

--- acmp/update.sh
+++ acmp/update.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 echo "Parsing table..."
+
 path=$( git rev-parse --show-toplevel )
-python $path/acmp/parser_top.py > $path/acmp/real_table_source.org 
+python $path/acmp/parser_top.py > $path/acmp/real_table_source.org
+
+emacs $path/acmp/real_table.org  --batch -f org-html-export-to-html --kill
+git add $path/acmp/real_table.org $path/acmp/real_table.html
+git commit -m "Table updating"
+git push
