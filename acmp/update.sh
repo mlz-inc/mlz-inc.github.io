5db62f5f1e25cdbbac3b9514ec276a926dcff2a9
mlz-inc <jury.mlz.inc@gmail.com>
2020-03-27 21:35:55 +0300

    Table updating

--- acmp/update.sh
+++ acmp/update.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 echo "Parsing table..."
 
-path=$( git rev-parse --show-toplevel )
+path="/home/amder/Projects/MLZ/mlzinc.github.io"
 python $path/acmp/parser_top.py > $path/acmp/real_table_source.org
 
+echo "Exporting org to html..."
 emacs $path/acmp/real_table.org  --batch -f org-html-export-to-html --kill
-git add $path/acmp/real_table.org $path/acmp/real_table.html
+rm $path/acmp/real_table.html\~
+
+echo "Pushing..."
+cd $path
+git add acmp/real_table.org acmp/real_table.html acmp/real_table_source.org
 git commit -m "Table updating"
 git push
